95388df101437cda036d352755c812c83f0f4db3
fabiano-moreira <fabiano.moreira.alves@gmail.com>
2019-11-23 15:09:12 -0300

    Alterados os arquivos em includes(nav.html) template (index.html e base.html) views.py e urls.py

--- projeto_django/urls.py
+++ projeto_django/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from projeto_django.core.views import index
+
 
 urlpatterns = [
+    path('', views.home, name='index'),
     path('admin/', admin.site.urls),
 ]
