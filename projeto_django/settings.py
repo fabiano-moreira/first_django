5f93f58feef6ce779269e884d7d89bc330612d67
fabiano-moreira <fabiano.moreira.alves@gmail.com>
2019-11-23 21:48:11 -0300

    alterada secret key no settings.py

--- projeto_django/settings.py
+++ projeto_django/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'jhbKPTA83HAbX9KzOEF60z7PW5hY1apa' #config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = False #config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
+ALLOWED_HOSTS = '*'#config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 
 # Application definition
